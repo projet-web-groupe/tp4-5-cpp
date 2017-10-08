1b56a694a4813a7af0c69f9db3e7e1422f20d16b
Roxog <gabrielcano@hotmail.fr>
2017-10-08 19:52:12 +0200

    .

--- Test.h
+++ Test.h
@@ -3,12 +3,6 @@
 
 class Test {
 public:
-	static bool testMobile1(void);
-	static bool testMobile2(void);
-	static bool testSimulation1(void);
-	static bool testSimulation2(void);
-	static bool testSimulation3(void);
-	static bool testSimulation4(void);
 	static bool testVecteur3D(void);
 	static double abs(double d);
 };
